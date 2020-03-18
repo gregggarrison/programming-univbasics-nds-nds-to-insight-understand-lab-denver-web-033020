c8e54d26a425264cd16821b9588ae14b055ac140
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-18 01:18:17 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,7 +10,6 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
- def print_first_directors_movie_titles
   movies_ar = directors_database[0][:movies]
   movies_ar_index = 0
   while movies_ar_index < movies_ar.count do
